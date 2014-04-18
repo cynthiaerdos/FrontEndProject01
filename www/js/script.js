fdfaebd58e1bf3a21aee36636b53857110d22958
Zoltan Erdos <zoltan.erdos@keytree.co.uk>
2014-04-18 18:46:31 +0100

    Load products from json store

--- www/js/script.js
+++ www/js/script.js
@@ -1,1 +1,9 @@
-﻿
+﻿function mainCtrl($scope, $http){
+    $scope.products = [];
+
+    $http.get("data/products.json").success(
+        function(data){
+            $scope.products = data;
+        }
+    );
+}
